70beb4bff884767d7ccda12d0183668f91f49abb
selimRU <selim.soha92@gmail.com>
2023-11-24 13:09:09 +0600

    index.js file created and mongodb connected with server

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc6s3b6.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        // collecins name
+        const reviewsCollection = client.db("skillMindsDB").collection("reviews");
+
+
+        app.get('api/v1/reviews', async (req, res) => {
+            const reviews = await reviewsCollection.find().toArray()
+            res.send(reviews)
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
