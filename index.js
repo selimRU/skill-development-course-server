be881a786854d2c1f49bfe9bb3b5d766c83e8a91
selimRU <selim.soha92@gmail.com>
2023-11-24 15:37:06 +0600

    reviews get api created and implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -16,9 +17,9 @@ app.use(express.json())
 
 
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc6s3b6.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -31,16 +32,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-
-        // collecins name
-        const reviewsCollection = client.db("skillMindsDB").collection("reviews");
+        const reviewsCollections = client.db("skillMindsDB").collection("reviews")
 
 
-        app.get('api/v1/reviews', async (req, res) => {
-            const reviews = await reviewsCollection.find().toArray()
+        app.get('/api/v1/reviews', async (req, res) => {
+            const reviews = await reviewsCollections.find().toArray()
+            console.log(reviews);
             res.send(reviews)
         })
-
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -50,6 +49,7 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
 
 
 app.listen(port, () => {
