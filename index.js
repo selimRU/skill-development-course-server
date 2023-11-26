4be2bbb522763a04303812b6606fe502a8c4318e
selimRU <selim.soha92@gmail.com>
2023-11-26 20:28:11 +0600

    payment,reject course,accept course api added and pushed

--- index.js
+++ index.js
@@ -31,16 +31,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const reviewsCollections = client.db("skillMindsDB").collection("reviews")
         const allCoursesCollections = client.db("skillMindsDB").collection("courses")
         const usersCollections = client.db("skillMindsDB").collection("users")
+        const teacherRequestCollections = client.db("skillMindsDB").collection("teachers")
 
         // course get api
         app.get('/api/v1/allCourses', async (req, res) => {
             const allCourses = await allCoursesCollections.find().toArray()
             console.log(allCourses);
             res.send(allCourses)
+        })
+
+        // teacher request get api
+        app.get('/api/v1/teacherRequest', async (req, res) => {
+            const allRequest = await teacherRequestCollections.find().toArray()
+            console.log(allRequest);
+            res.send(allRequest)
         })
 
         // course get api by id
@@ -67,14 +75,82 @@ async function run() {
         })
 
         // users get api
+        app.get('/api/v1/getUsers/count', async (req, res) => {
+            const usersCount = await usersCollections.estimatedDocumentCount()
+            const couresCount = await allCoursesCollections.estimatedDocumentCount()
+            res.send({ usersCount, couresCount })
+        })
+
+        // admin get api
         app.get('/api/v1/getUsers/admin/:email', async (req, res) => {
             const email = req.params.email
             const query = { email: email }
             const user = await usersCollections.findOne(query)
-            const admin = user?.role === 'admin'
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
             console.log(admin);
             res.send({ admin })
         })
+
+        // admin get api
+        app.get('/api/v1/getCourses/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await allCoursesCollections.find(query).toArray()
+            res.send(result)
+        })
+
+        // course deletet api
+        app.delete('/api/v1/deleteCourse/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await allCoursesCollections.deleteOne(query)
+            res.send(result)
+        })
+
+        // teacher's course update api
+        app.patch('/api/v1/updateCourse/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const course = req.body
+            const updatedCourse = {
+                $set: {
+                    name: course.name,
+                    image: course.image,
+                    title: course.title,
+                    email: course.email,
+                    price: course.price,
+                    description: course.description
+                }
+            }
+            const result = await allCoursesCollections.updateOne(filter, updatedCourse)
+            res.send(result)
+        })
+
+
+        // teacher get api
+        app.get('/api/v1/getUser/teacher/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await teacherRequestCollections.findOne(query)
+            let teacher = false
+            if (user) {
+                teacher = user?.role === 'accepted'
+            }
+            console.log(teacher);
+            res.send({ teacher })
+        })
+
+        // courses post api
+        app.post('/api/v1/teacherRequestPost', async (req, res) => {
+            const request = req.body
+            const result = await teacherRequestCollections.insertOne(request)
+            console.log(result);
+            res.send(result)
+        })
+
         // users post api
         app.post('/api/v1/createUsers', async (req, res) => {
             const user = req.body
@@ -83,7 +159,15 @@ async function run() {
             res.send(result)
         })
 
-        // make admin api api
+        // add course post api
+        app.post('/api/v1/addCourse', async (req, res) => {
+            const user = req.body
+            const result = await allCoursesCollections.insertOne(user)
+            console.log(result);
+            res.send(result)
+        })
+
+        // update admin api 
         app.patch('/api/v1/makeAdmin/:id', async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
@@ -97,19 +181,111 @@ async function run() {
             res.send(result)
         })
 
-        // make teacher api
-        app.patch('/api/v1/makeTeacher/:id', async (req, res) => {
+        // update course by admin rejected api 
+        app.patch('/api/v1/courseRejected/:id', async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
                 $set: {
-                    role: 'teacher'
+                    status: 'rejected'
                 }
             }
-            const result = await usersCollections.updateOne(filter, updatedDoc)
+            const result = await allCoursesCollections.updateOne(filter, updatedDoc)
             console.log(result);
             res.send(result)
         })
+
+        // update course by admin accepted api 
+        app.patch('/api/v1/courseAccepted/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: 'accepted'
+                }
+            }
+            const result = await allCoursesCollections.updateOne(filter, updatedDoc)
+            console.log(result);
+            res.send(result)
+        })
+
+        // update as teacher accepted api
+        app.patch('/api/v1/requestAccepted/:name', async (req, res) => {
+            const name = req.params.name
+            // console.log(filter);
+            const filter = { name: name }
+            // const filter = { _id: new ObjectId(id) }
+            const updateTeacher = {
+                $set: {
+                    role: 'accepted'
+                }
+            }
+            const updateUser = {
+                $set: {
+                    role: 'teacher'
+                }
+            }
+            const updatedTeacher = await teacherRequestCollections.updateOne(filter, updateTeacher)
+            const updatedUser = await usersCollections.updateOne(filter, updateUser)
+            res.send({ updatedTeacher, updatedUser })
+        })
+
+        // update as teacher rejected api
+        app.patch('/api/v1/requestRejected/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'rejected'
+                }
+            }
+            const result = await teacherRequestCollections.updateOne(filter, updatedDoc)
+            console.log(result);
+            res.send(result)
+        })
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: parseInt(price * 100),
+                currency: "usd",
+                "payment_method_types": [
+                    "card"
+                ],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        // pament history
+        app.post("/api/v1/payment",async (req, res) => {
+            const payment = req.body;
+            const paymentResult = await paymentCollections.insertOne(payment)
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            }
+            const deleteResults = await cartsCollections.deleteMany(query)
+            console.log(paymentResult, deleteResults);
+            res.send({ paymentResult, deleteResults });
+        });
+        // // make teacher api
+        // app.patch('/api/v1/makeTeacher/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const filter = { _id: new ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set: {
+        //             role: 'accepted'
+        //         }
+        //     }
+        //     const result = await usersCollections.updateOne(filter, updatedDoc)
+        //     console.log(result);
+        //     res.send(result)
+        // })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
